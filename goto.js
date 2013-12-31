b1c5fe8756ffa2d4513945dd5ae888e4522b250b
Daniel Spangler <daniel.spangler@homes.com>
2013-12-31 14:52:33 -0500

    Started putting a toolbar button in place, added support for stripping out the host in a selection

--- goto.js
+++ goto.js
@@ -1,4 +1,5 @@
 
+
 function openLocal(info, tab) {
   openIn(info, tab, "http://djspang.homes.com");
 }
@@ -7,19 +8,34 @@ function openStaging(info, tab) {
   openIn(info, tab, "http://staging.homes.com");
 }
 
+function openProd(info, tab) {
+	  openIn(info, tab, "http://www.homes.com");
+}
 
 function openIn(info, tab, prefix) {
-  var newURL = prefix + info.selectionText.trim();
-  newURL = newURL.replace("//", "/");
+  var selection = info.selectionText.trim(); // remove whitespace
+  if (selection.indexOf('/')!=0) { // if the selection doesn't have a starting forward slash, make it so
+  	selection = '/' + selection;
+  }
+
+  var hostTest = /.*homes.com(.*)/;
+  selection = selection.replace(hostTest, "$1");
+
+  var newURL = prefix + selection;
+  newURL = newURL.replace("//", "/"); // remove any duplicate forward slashes if they exist
   chrome.tabs.create({ index: tab.index+1, url: newURL });  
 }
 
 
-var id = chrome.contextMenus.create({"title": "Open in Local Dev", "contexts":["selection"],
+var id = chrome.contextMenus.create({"title": "Local Dev", "contexts":["selection"],
                                      "onclick": openLocal});
 
 
-var id = chrome.contextMenus.create({"title": "Open in Staging", "contexts":["selection"],
+var id = chrome.contextMenus.create({"title": "Staging", "contexts":["selection"],
                                      "onclick": openStaging});
+
+var id = chrome.contextMenus.create({"title": "Prod", "contexts":["selection"],
+	                                     "onclick": openProd});
+
 
 
